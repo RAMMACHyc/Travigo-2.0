32fc574f31c9de0be441b6b8bc95832b41c5ac55
RAMMACHyc <sadik.ram12@gmail.com>
2022-12-09 17:45:25 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+require_once './autoload.php';
+require_once './views/includes/header.php';
+
+
+
+
+
+$home = new HomeController();
+$pages = ['index','about','touls','travi','home'];
+// $home->index('delete');
+if (isset($_GET['page'])) {
+    if (in_array($_GET['page'],$pages)) {
+        $page = $_GET['page'];
+        $home->index($page);
+    }else{
+        include('views/includes/404.php');
+    }
+}else{
+    $home->index('index');
+}
+?>
+<?php
+require_once './views/includes/footer.php';
+?>
